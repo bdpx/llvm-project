f5d90f9e92d33f0d60c64318aca159c8056a0888
Dmitry Buvaylo <moxoron@gmail.com>
2022-10-24 21:09:09 +0300

    [Postrisc] llvm backend (llvm/lib/Target/Postrisc)

---
+++ llvm/lib/Target/Postrisc/MCTargetDesc/PostriscMCExpr.cpp
@@ -0,0 +1,100 @@
+//===-- PostriscMCExpr.cpp - Postrisc specific MC expression classes --------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the implementation of the assembly expression modifiers
+// accepted by the Postrisc architecture (e.g. "%hi", "%lo", ...).
+//
+//===----------------------------------------------------------------------===//
+
+#include "PostriscMCExpr.h"
+#include "llvm/BinaryFormat/ELF.h"
+#include "llvm/MC/MCAssembler.h"
+#include "llvm/MC/MCContext.h"
+#include "llvm/MC/MCObjectStreamer.h"
+#include "llvm/MC/MCSymbolELF.h"
+#include "llvm/MC/MCValue.h"
+#include "llvm/Support/Casting.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "postriscmcexpr"
+
+const PostriscMCExpr *PostriscMCExpr::create(Specifier S, const MCExpr *Expr,
+                                       MCContext &Ctx) {
+  return new (Ctx) PostriscMCExpr(S, Expr);
+}
+
+void PostriscMCExpr::printImpl(raw_ostream &OS, const MCAsmInfo *MAI) const {
+
+  bool closeParen = printSpecifier(OS, specifier);
+
+  const MCExpr *Expr = getSubExpr();
+  Expr->print(OS, MAI);
+
+  if (closeParen)
+    OS << ')';
+}
+
+bool PostriscMCExpr::printSpecifier(raw_ostream &OS, Specifier S) {
+  switch (S) {
+  case VK_NONE:     return false;
+#define POSTRISC_VARIANT_KIND_TABLE_PRINT(NAME, FUNCNAME) \
+  case VK_##NAME: OS << "%" #FUNCNAME "("; return true;
+
+  POSTRISC_USUAL_KIND_TABLE(POSTRISC_VARIANT_KIND_TABLE_PRINT)
+  POSTRISC_VARIANT_KIND_TABLE(POSTRISC_VARIANT_KIND_TABLE_PRINT)
+
+#undef POSTRISC_VARIANT_KIND_TABLE_PRINT
+  }
+  llvm_unreachable("Unhandled PostriscMCExpr::Specifier");
+}
+
+PostriscMCExpr::Specifier PostriscMCExpr::parseSpecifier(StringRef name)
+{
+  return StringSwitch<PostriscMCExpr::Specifier>(name)
+#define POSTRISC_VARIANT_KIND_TABLE_NAMECASE(NAME, FUNCNAME) \
+  .Case(#FUNCNAME, VK_##NAME)
+
+  POSTRISC_VARIANT_KIND_TABLE(POSTRISC_VARIANT_KIND_TABLE_NAMECASE)
+
+#undef POSTRISC_VARIANT_KIND_TABLE_NAMECASE
+    .Default(VK_NONE);
+}
+
+Postrisc::Fixups
+PostriscMCExpr::getFixupKind(PostriscMCExpr::Specifier Kind)
+{
+  switch (Kind) {
+  default: llvm_unreachable("Unhandled PostriscMCExpr::Specifier");
+#define POSTRISC_VARIANT_KIND_TABLE_FIXUP(NAME, FUNCNAME) \
+  case VK_##NAME: return Postrisc::FIXUP_POSTRISC_##NAME;
+
+  POSTRISC_USUAL_KIND_TABLE(POSTRISC_VARIANT_KIND_TABLE_FIXUP)
+
+#undef POSTRISC_VARIANT_KIND_TABLE_FIXUP
+
+#define POSTRISC_VARIANT_KIND_TABLE_FIXUP(NAME, FUNCNAME) \
+  case VK_##NAME: return Postrisc::FIXUP_POSTRISC_A_##NAME;
+
+  POSTRISC_VARIANT_KIND_TABLE(POSTRISC_VARIANT_KIND_TABLE_FIXUP)
+
+#undef POSTRISC_VARIANT_KIND_TABLE_FIXUP
+  }
+}
+
+bool PostriscMCExpr::evaluateAsRelocatableImpl(MCValue &Res,
+                                            const MCAssembler *Asm) const {
+  if (!getSubExpr()->evaluateAsRelocatable(Res, Asm))
+    return false;
+  Res.setSpecifier(specifier);
+  return true;
+}
+
+void PostriscMCExpr::visitUsedExpr(MCStreamer &Streamer) const {
+  Streamer.visitUsedExpr(*getSubExpr());
+}
---
+++ llvm/lib/Target/Postrisc/PostriscSubtarget.cpp
@@ -0,0 +1,62 @@
+//===-- PostriscSubtarget.cpp - POSTRISC Subtarget Information ------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the POSTRISC specific subclass of TargetSubtargetInfo.
+//
+//===----------------------------------------------------------------------===//
+
+#include "PostriscSubtarget.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/MC/TargetRegistry.h"
+#include "llvm/Support/MathExtras.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "postrisc-subtarget"
+
+#define GET_SUBTARGETINFO_TARGET_DESC
+#define GET_SUBTARGETINFO_CTOR
+#include "PostriscGenSubtargetInfo.inc"
+
+void PostriscSubtarget::anchor() { }
+
+PostriscSubtarget &PostriscSubtarget::initializeSubtargetDependencies(
+    StringRef CPU, StringRef TuneCPU, StringRef FS) {
+  // Determine default and user specified characteristics
+  std::string CPUName = std::string(CPU);
+  if (CPUName.empty())
+    CPUName = "pv1";
+
+  if (TuneCPU.empty())
+    TuneCPU = CPUName;
+
+  // Parse features string.
+  ParseSubtargetFeatures(CPUName, TuneCPU, FS);
+
+  return *this;
+}
+
+PostriscSubtarget::PostriscSubtarget(const StringRef &CPU, const StringRef &TuneCPU,
+                               const StringRef &FS, const TargetMachine &TM,
+                               bool is64Bit)
+    : PostriscGenSubtargetInfo(TM.getTargetTriple(), CPU, TuneCPU, FS),
+      TargetTriple(TM.getTargetTriple()), Is64Bit(is64Bit),
+      InstrInfo(initializeSubtargetDependencies(CPU, TuneCPU, FS)),
+      TLInfo(TM, *this), FrameLowering(*this) {}
+
+int PostriscSubtarget::getAdjustedFrameSize(int frameSize) const {
+  // All 64-bit stack frames must be 16-byte aligned, and must reserve space
+  // for 6 outgoing arguments at %sp+BIAS..%sp+BIAS+128.
+  // whether they are used or not. LowerCall_64 takes care of that.
+  frameSize = alignTo(frameSize, 16);
+  return frameSize;
+}
+
+bool PostriscSubtarget::enableMachineScheduler() const {
+  return true;
+}
