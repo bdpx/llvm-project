f778e6a6d4455e6ce2bc396766eccf74fb53f874
Dmitry Buvaylo <moxoron@gmail.com>
2022-10-24 21:09:09 +0300

    [Postrisc] llvm backend (llvm/lib/Target/Postrisc)

---
+++ llvm/lib/Target/Postrisc/TargetInfo/PostriscTargetInfo.cpp
@@ -0,0 +1,21 @@
+//===-- PostriscTargetInfo.cpp - Postrisc Target Implementation -----------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "TargetInfo/PostriscTargetInfo.h"
+#include "llvm/MC/TargetRegistry.h"
+using namespace llvm;
+
+Target &llvm::getThePostriscTarget() {
+  static Target ThePostriscTarget;
+  return ThePostriscTarget;
+}
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializePostriscTargetInfo() {
+  RegisterTarget<Triple::postrisc, /*HasJIT=*/true> X(getThePostriscTarget(),
+                                                    "postrisc", "Postrisc", "Postrisc");
+}
