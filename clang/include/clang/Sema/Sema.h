bb935f979bdd03b2c0bff10976a79a7dc94433b6
Dmitry Buvaylo <moxoron@gmail.com>
2022-10-26 13:31:08 +0300

    [Postrisc] clang builtins (clang/include/clang/Basic|Sema, clang/lib/CodeGen|Sema|Headers)

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -172,6 +172,7 @@ class SemaObjC;
 class SemaOpenACC;
 class SemaOpenCL;
 class SemaOpenMP;
+class SemaPostrisc;
 class SemaPPC;
 class SemaPseudoObject;
 class SemaRISCV;
@@ -1456,6 +1457,11 @@ public:
     return *M68kPtr;
   }
 
+  SemaPostrisc &Postrisc() {
+    assert(PostriscPtr);
+    return *PostriscPtr;
+  }
+
   SemaMIPS &MIPS() {
     assert(MIPSPtr);
     return *MIPSPtr;
@@ -1586,6 +1592,7 @@ private:
   std::unique_ptr<SemaOpenACC> OpenACCPtr;
   std::unique_ptr<SemaOpenCL> OpenCLPtr;
   std::unique_ptr<SemaOpenMP> OpenMPPtr;
+  std::unique_ptr<SemaPostrisc> PostriscPtr;
   std::unique_ptr<SemaPPC> PPCPtr;
   std::unique_ptr<SemaPseudoObject> PseudoObjectPtr;
   std::unique_ptr<SemaRISCV> RISCVPtr;
