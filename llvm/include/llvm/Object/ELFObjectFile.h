a0eb27ad9a9523c4c92b6fe98de8ff342a18a2ec
Dmitry Buvaylo <moxoron@gmail.com>
2022-10-26 13:36:14 +0300

    [Postrisc] llvm: ELF definitions (llvm/include/llvm/BinaryFormat, llvm/lib/Object)

--- llvm/include/llvm/Object/ELFObjectFile.h
+++ llvm/include/llvm/Object/ELFObjectFile.h
@@ -1347,6 +1347,8 @@ StringRef ELFObjectFile<ELFT>::getFileFormatName() const {
       return "elf64-ve";
     case ELF::EM_LOONGARCH:
       return "elf64-loongarch";
+    case ELF::EM_POSTRISC:
+      return "elf64-postrisc";
     default:
       return "elf64-unknown";
     }
@@ -1450,6 +1452,9 @@ template <class ELFT> Triple::ArchType ELFObjectFile<ELFT>::getArch() const {
 
   case ELF::EM_XTENSA:
     return Triple::xtensa;
+
+  case ELF::EM_POSTRISC:
+    return Triple::postrisc;
 
   default:
     return Triple::UnknownArch;
