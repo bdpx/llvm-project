6d4e7798d39d153df487ef6b2116e55ad4aed0ef
Dmitry Buvaylo <moxoron@gmail.com>
2022-10-26 13:37:46 +0300

    [Postrisc] llvm: target, triples

--- llvm/lib/TargetParser/Triple.cpp
+++ llvm/lib/TargetParser/Triple.cpp
@@ -84,6 +84,7 @@ StringRef Triple::getArchTypeName(ArchType Kind) {
   case x86_64:         return "x86_64";
   case xcore:          return "xcore";
   case xtensa:         return "xtensa";
+  case postrisc:       return "postrisc";
   }
 
   llvm_unreachable("Invalid ArchType!");
@@ -228,6 +229,8 @@ StringRef Triple::getArchTypePrefix(ArchType Kind) {
   case dxil:        return "dx";
 
   case xtensa:      return "xtensa";
+
+  case postrisc:    return "postrisc";
   }
 }
 
@@ -451,6 +454,7 @@ Triple::ArchType Triple::getArchTypeForLLVMName(StringRef Name) {
     .Case("loongarch64", loongarch64)
     .Case("dxil", dxil)
     .Case("xtensa", xtensa)
+    .Case("postrisc", postrisc)
     .Default(UnknownArch);
 }
 
@@ -597,6 +601,7 @@ static Triple::ArchType parseArch(StringRef ArchName) {
                  "dxilv1.4", "dxilv1.5", "dxilv1.6", "dxilv1.7", "dxilv1.8",
                  Triple::dxil)
           .Case("xtensa", Triple::xtensa)
+          .Case("postrisc", Triple::postrisc)
           .Default(Triple::UnknownArch);
 
   // Some architectures require special parsing logic just to compute the
@@ -928,6 +933,7 @@ static Triple::ObjectFormatType getDefaultFormat(const Triple &T) {
   case Triple::ve:
   case Triple::xcore:
   case Triple::xtensa:
+  case Triple::postrisc:
     return Triple::ELF;
 
   case Triple::ppc64:
@@ -1643,6 +1649,7 @@ unsigned Triple::getArchPointerBitWidth(llvm::Triple::ArchType Arch) {
   case llvm::Triple::ve:
   case llvm::Triple::wasm64:
   case llvm::Triple::x86_64:
+  case llvm::Triple::postrisc:
     return 64;
   }
   llvm_unreachable("Invalid architecture value");
@@ -1671,6 +1678,7 @@ Triple Triple::get32BitArchVariant() const {
   case Triple::msp430:
   case Triple::systemz:
   case Triple::ve:
+  case Triple::postrisc:
     T.setArch(UnknownArch);
     break;
 
@@ -1784,6 +1792,7 @@ Triple Triple::get64BitArchVariant() const {
   case Triple::ve:
   case Triple::wasm64:
   case Triple::x86_64:
+  case Triple::postrisc:
     // Already 64-bit.
     break;
 
@@ -1863,6 +1872,7 @@ Triple Triple::getBigEndianArchVariant() const {
   // drop any arch suffixes.
   case Triple::arm:
   case Triple::thumb:
+  case Triple::postrisc:
     T.setArch(UnknownArch);
     break;
 
@@ -1967,6 +1977,7 @@ bool Triple::isLittleEndian() const {
   case Triple::x86_64:
   case Triple::xcore:
   case Triple::xtensa:
+  case Triple::postrisc:
     return true;
   default:
     return false;
