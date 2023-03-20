c8fe24fc1d539d9cbef7649c90140c7340d3c80e
Dmitry Buvaylo <moxoron@gmail.com>
2023-03-20 18:08:44 +0200

    [Postrisc] CXXABI (clang/include/clang/Basic)

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -97,6 +97,7 @@ static CGCXXABI *createCXXABI(CodeGenModule &CGM) {
   case TargetCXXABI::WatchOS:
   case TargetCXXABI::GenericMIPS:
   case TargetCXXABI::GenericItanium:
+  case TargetCXXABI::GenericPostrisc:
   case TargetCXXABI::WebAssembly:
   case TargetCXXABI::XL:
     return CreateItaniumCXXABI(CGM);
