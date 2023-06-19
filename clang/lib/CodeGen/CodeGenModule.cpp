2dd46f2f34e6dc1ee3b81b39b2c074dd6aa35cc6
Dmitry Buvailo <moxoron@gmail.com>
2023-06-19 23:08:57 +0300

    [Postrisc] clang target codegen info (clang/lib/Codegen[/Targets], draft)

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -327,6 +327,8 @@ createTargetCodeGenInfo(CodeGenModule &CGM) {
     return createLoongArchTargetCodeGenInfo(
         CGM, Target.getPointerWidth(LangAS::Default), ABIFRLen);
   }
+  //case llvm::Triple::postrisc:
+  //  return createPostriscTargetCodeGenInfo(CGM);
   }
 }
 
